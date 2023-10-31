c1d8322fbe4835f7c072deec7f065f8b58a6a994
SantoshOfficial22 <santoshkumarofficial22@gmail.com>
2023-10-31 16:33:51 +0530

    first commit

---
+++ ASP_NET_Simple_Login_and_CRUD_MVC_Architecture/Proper_LARCD1A_Trail1/BusinessAccessLayer/Implementation/LoginAndRegistration.cs
@@ -0,0 +1,62 @@
+﻿using BusinessAccessLayer.Abstract;
+using DataAccessLayer.Entity_DB;
+using ModelAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer.Implementation
+{
+    public class LoginAndRegistration : ILoginAndRegistration
+    {
+        private PracticeEntities db = new PracticeEntities();
+        public bool Register(LoginAndRegistrationModel loginAndRegistrationModel)
+        {
+            Trail1LoginAndRegis trail1LoginAndRegis = new Trail1LoginAndRegis
+            {
+                Name = loginAndRegistrationModel.Name,
+                Email = loginAndRegistrationModel.Email,
+                Password = loginAndRegistrationModel.Password
+            };
+            db.Trail1LoginAndRegis.Add(trail1LoginAndRegis);
+            var Result = db.SaveChanges();
+            if(Result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public bool Login(LoginAndRegistrationModel loginAndRegistrationModel)
+        {
+            var CheckDetails = (from userlist in db.Trail1LoginAndRegis
+                              where userlist.Email == loginAndRegistrationModel.Email && userlist.Password == loginAndRegistrationModel.Password
+                              select new
+                              {
+                                  userlist.Email,
+                                  userlist.Password
+                              }).ToList();
+            if (CheckDetails.FirstOrDefault() != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public int Temp()
+        {
+
+            int a = 10;
+            return a;
+        }
+    }
+}
---
+++ ASP_NET_Simple_Login_and_CRUD_MVC_Architecture/Proper_LARCD1A_Trail1/Proper_LARCD1A_Trail1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proper_LARCD1A_Trail1.MvcApplication" Language="C#" %>
